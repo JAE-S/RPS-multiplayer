e06355fb4b60c8b5d1b7bce21275e2267b679b22
Jessie S <jessie.sholler@gmail.com>
2019-08-11 19:21:57 -0400

    firebase initial setup - html javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,17 +9,17 @@ RPS - Multiplayer Game
 Firebase configuration
 ========================================
 */
-// var firebaseConfig = {
-//     apiKey: "AIzaSyDjD6f9WuqO9tlDUrU_Pb_HwMvFOGrOE2M",
-//     authDomain: "rps-multiplayer-e6ab6.firebaseapp.com",
-//     databaseURL: "https://rps-multiplayer-e6ab6.firebaseio.com",
-//     projectId: "rps-multiplayer-e6ab6",
-//     storageBucket: "",
-//     messagingSenderId: "1062565958363",
-//     appId: "1:1062565958363:web:ec1df26c1943f070"
-//   };
-  // Initialize Firebase
-// firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyDjD6f9WuqO9tlDUrU_Pb_HwMvFOGrOE2M",
+    authDomain: "rps-multiplayer-e6ab6.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-e6ab6.firebaseio.com",
+    projectId: "rps-multiplayer-e6ab6",
+    storageBucket: "",
+    messagingSenderId: "1062565958363",
+    appId: "1:1062565958363:web:ec1df26c1943f070"
+  };
+
+firebase.initializeApp(config);    // Initialize Firebase
 
 /*
 ========================================
@@ -27,6 +27,7 @@ GLOBAL VARIABLES
 ========================================
 */
 
+var database = firebase.database(); // Get a reference to the database service
 
 /*
 ========================================
