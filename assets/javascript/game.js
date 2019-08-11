869411439afe1b5a9a3fcf94b30536d0c134edfe
Jessie S <jessie.sholler@gmail.com>
2019-08-11 18:40:55 -0400

    initial document setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+/*
+========================================
+RPS - Multiplayer Game 
+========================================
+*/
+
+/*
+========================================
+Firebase configuration
+========================================
+*/
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDjD6f9WuqO9tlDUrU_Pb_HwMvFOGrOE2M",
+//     authDomain: "rps-multiplayer-e6ab6.firebaseapp.com",
+//     databaseURL: "https://rps-multiplayer-e6ab6.firebaseio.com",
+//     projectId: "rps-multiplayer-e6ab6",
+//     storageBucket: "",
+//     messagingSenderId: "1062565958363",
+//     appId: "1:1062565958363:web:ec1df26c1943f070"
+//   };
+  // Initialize Firebase
+// firebase.initializeApp(config);
+
+/*
+========================================
+GLOBAL VARIABLES 
+========================================
+*/
+
+
+/*
+========================================
+Fire-base Authentication 
+========================================
+*/
+
+/*
+========================================
+Fire-base Chat functionality
+========================================
+*/
+
+
+
+$('#active-game').hide(); // Hides active game section
